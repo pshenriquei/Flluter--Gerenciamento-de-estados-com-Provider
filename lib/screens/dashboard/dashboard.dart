897b90a49aea4aeb829212648f661dca2be7c1ff
Pedro Henrique Ignacio Sobrinho <pedrohenriquei@live.com>
2021-08-26 19:25:19 -0300

    Gerenciando as transferenciais
    -Como gerenciar múltiplas classes com o MultipleProvider
    -Adicionar uma Transferência através do Provider
    -Fazer com que essa transferência esteja vinculada com o saldo da conta

--- lib/screens/dashboard/dashboard.dart
+++ lib/screens/dashboard/dashboard.dart
@@ -1,5 +1,6 @@
-import 'package:bytebank/models/saldo.dart';
 import 'package:bytebank/screens/dashboard/saldo_card.dart';
+import 'package:bytebank/screens/deposito/formulario.dart';
+import 'package:bytebank/screens/transferencia/formulario.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
@@ -9,9 +10,40 @@ class Dashboard extends StatelessWidget {
       appBar: AppBar(
         title: const Text('ByteBank'),
       ),
-      body: Align(
-        alignment: Alignment.topCenter,
-        child: SaldoCard(),
+      body: ListView(
+        children: <Widget>[
+          Align(
+            alignment: Alignment.topCenter,
+            child: SaldoCard(),
+          ),
+          ButtonBar(
+            alignment: MainAxisAlignment.center,
+            children: <Widget>[
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.green),
+                  child: Text('Receber depósito'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return FormularioDeposito();
+                      }),
+                    );
+                  }),
+              ElevatedButton(
+                  style: ElevatedButton.styleFrom(primary: Colors.green),
+                  child: Text('Nova transferência'),
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) {
+                        return FormularioTransferencia();
+                      }),
+                    );
+                  }),
+            ],
+          ),
+        ],
       ),
     );
   }
